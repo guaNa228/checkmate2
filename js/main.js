366c8dfc70e7a94039d2a3f122157332ef414d48
G <frolovgeorgiy@gmail.com>
2021-12-21 04:47:33 +0300

    language_reverse update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+import {langData} from './reverse-language.js';
+
+
+/* language */
+const translateable = ["ru", "uk", "be", "kk"];
+
+const userLang = navigator.language || navigator.userLanguage;
+if (translateable.includes(userLang)) translate();
+
+function translate() {
+    let elements = document.getElementsByTagName("*");
+    let tmp;
+    let neededCounter = 0;
+    for (let i = 0; i<elements.length; i++) {
+        if (elements[i].tagName!="TITLE" && elements[i].innerText && elements[i].innerText.indexOf("\n")==-1 && 
+        !elements[i].classList.contains("parters-grid__item") && !elements[i].parentElement.classList.contains("parters-grid__item") &&
+        elements[i].tagName!="DIV" && elements[i].tagName!="H3" && !elements[i].classList.contains("footer-info__email")
+         || (elements[i].parentElement && elements[i].tagName=="P" && elements[i].parentElement.classList.contains("partner-grid__item_become"))) {
+            tmp = langData[neededCounter];
+            langData[neededCounter] = elements[i].innerHTML;
+            elements[i].innerHTML = tmp;
+            neededCounter++;
+        }
+    }
+}
+
+/* Mobile menu */
 const menuOpenBtn = document.querySelector(".header__top-panel__menu-button");
 const menuCloseBtn = document.querySelector(".mobile-menu__close-button");
 const mobileMenu = document.querySelector(".mobile-menu");
